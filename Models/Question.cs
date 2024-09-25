b47dfeeadf95426698166cbbb8b07237c032b835
AurelEnea <aurelenea10@gmail.com>
2024-09-25 22:14:11 +0000

    ValidationComplete

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SurveyAuth.Models
 {
     public class Question
     {
         public int Id {get; set;}
         public int SurveyId {get; set;}
+        [Required(ErrorMessage = "Question text is required")]
         public string Text {get; set;}
         public string Description {get; set;}
         public Survey Survey {get; set;}
--- obj/Debug/net8.0/SurveyAuth.AssemblyInfo.cs
+++ obj/Debug/net8.0/SurveyAuth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4738846f6e18210edd3c9127e3138057a19397af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7868641cc9badb2a409e887e9f21c9c28b797c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
